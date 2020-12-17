a4119b55f1c025a8a26a469fb38280cc609c8cba
Adewale Adeleye <Adewagold@yahoo.com>
2020-12-17 23:42:55 +0100

    Initial commit

---
+++ src/main/java/com/edunge/srtool/SrToolApplication.java
@@ -0,0 +1,13 @@
+package com.edunge.srtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SrToolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edunge/srtool/SrToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edunge.srtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
