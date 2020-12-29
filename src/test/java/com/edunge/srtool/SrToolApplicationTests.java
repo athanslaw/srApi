5ba83391527aec340be17cb8498f8236c30a597d
Adewale Adeleye <Adewagold@yahoo.com>
2020-12-29 14:26:57 +0100

    Update app.properties

--- src/test/java/com/edunge/srtool/SrToolApplicationTests.java
+++ src/test/java/com/edunge/srtool/SrToolApplicationTests.java
@@ -2,8 +2,10 @@ package com.edunge.srtool;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("local")
 class SrToolApplicationTests {
 
 	@Test
