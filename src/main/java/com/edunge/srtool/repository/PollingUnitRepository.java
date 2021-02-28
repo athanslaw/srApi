8c7fb3a174073dc099c5c2edc3a57be16915670f
Adewale Adeleye <Adewagold@yahoo.com>
2021-02-28 21:56:02 +0100

    Polling unit filters

--- src/main/java/com/edunge/srtool/repository/PollingUnitRepository.java
+++ src/main/java/com/edunge/srtool/repository/PollingUnitRepository.java
@@ -1,8 +1,6 @@
 package com.edunge.srtool.repository;
 
-import com.edunge.srtool.model.Lga;
-import com.edunge.srtool.model.PollingUnit;
-import com.edunge.srtool.model.Ward;
+import com.edunge.srtool.model.*;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -12,4 +10,6 @@ public interface PollingUnitRepository extends JpaRepository<PollingUnit, Long> 
     List<PollingUnit> findByNameStartingWith(String name);
     List<PollingUnit> findByWard(Ward ward);
     List<PollingUnit> findByLga(Lga lga);
+    List<PollingUnit> findByState(State state);
+    List<PollingUnit> findBySenatorialDistrict(SenatorialDistrict senatorialDistrict);
 }
--- src/main/java/com/edunge/srtool/service/PollingUnitService.java
+++ src/main/java/com/edunge/srtool/service/PollingUnitService.java
@@ -10,6 +10,13 @@ public interface PollingUnitService {
     PollingUnitResponse findPollingUnitById(Long id) throws NotFoundException;
     PollingUnitResponse findPollingUnitByCode(String code) throws NotFoundException;
     PollingUnitResponse updatePollingUnit(Long id, PollingUnitDto pollingUnitDto) throws NotFoundException;
+
+    PollingUnitResponse findByLga(Long lgaCode) throws NotFoundException;
+
+    PollingUnitResponse findByState(Long stateCode) throws NotFoundException;
+
+    PollingUnitResponse findBySenatorialDistrict(Long senatorialDistrictCode) throws NotFoundException;
+
     PollingUnitResponse deletePollingUnitById(Long id) throws NotFoundException;
     PollingUnitResponse findAll() ;
     PollingUnitResponse filterByName(String name) throws NotFoundException;
