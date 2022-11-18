e179704198041195ffd0be4e14895fdab7459f31
Athanasius.Lawrence-dev <Athanasius.Lawrence@ISW-ENG-AL>
2022-11-18 08:50:21 +0100

    Update

--- src/test/java/com/edunge/srtool/SrToolApplicationTests.java
+++ src/test/java/com/edunge/srtool/SrToolApplicationTests.java
@@ -1,33 +1,15 @@
 package com.edunge.srtool;
 
-import com.edunge.srtool.service.impl.CallServiceImpl;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-import java.net.URISyntaxException;
-
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-
 @SpringBootTest
-//@ActiveProfiles("local")
+@ActiveProfiles("local")
 class SrToolApplicationTests {
-//
-//	@Autowired
-//	CallServiceImpl callService;
+
 	@Test
 	void contextLoads() {
 	}
-/*
-	@Test
-	void callTwilio(){
-		try {
-			callService.makeCall("+2347068551023");
-		}catch (URISyntaxException e){
-			System.out.println("Here");
-		}
-		assertNotNull("requeryAccount");
-	}*/
 
 }
